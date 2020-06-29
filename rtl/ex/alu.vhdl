025e672be57168dd91ceb609a3642a0f70c74fb0
Dominik Salvet <dominik.salvet@gmail.com>
2020-06-29 14:46:47 +0200

    add another vhdl files

--- rtl/ex/alu.vhdl
+++ rtl/ex/alu.vhdl
@@ -1,4 +1,19 @@
-----------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
 -- Copyright 2020 Dominik Salvet
 -- github.com/dominiksalvet/risc63
-----------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+use work.alu_pkg.all;
+
+entity alu is
+    port (
+        i_opcode: in std_ulogic_vector(3 downto 0);
+        i_a_operand: in std_ulogic_vector(63 downto 0);
+        i_b_operand: in std_ulogic_vector(63 downto 0);
+        o_result: out std_ulogic_vector(63 downto 0)
+    );
+end entity alu;
