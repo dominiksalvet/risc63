23cb52ae99031c2294e340bf5e84acec3667bc09
Dominik Salvet <dominik.salvet@gmail.com>
2020-06-30 22:35:54 +0200

    fix missing library in risc63 package

--- rtl/risc63_pkg.vhdl
+++ rtl/risc63_pkg.vhdl
@@ -3,6 +3,9 @@
 -- github.com/dominiksalvet/risc63
 --------------------------------------------------------------------------------
 
+library ieee;
+use ieee.std_logic_1164.all;
+
 package risc63_pkg is
     -- value of PC after reset
     constant c_PC_RST: std_ulogic_vector(62 downto 0) := (others => '0');
