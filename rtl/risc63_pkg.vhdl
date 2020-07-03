85ecde935f73d051fb077760a0f5d3d5246e5194
Dominik Salvet <dominik.salvet@gmail.com>
2020-07-03 19:58:04 +0200

    add NOP instruction constant

--- rtl/risc63_pkg.vhdl
+++ rtl/risc63_pkg.vhdl
@@ -9,4 +9,6 @@ use ieee.std_logic_1164.all;
 package risc63_pkg is
     -- value of PC after reset
     constant c_PC_RST: std_ulogic_vector(62 downto 0) := (others => '0');
+
+    constant c_NOP_INST: std_ulogic_vector(15 downto 0) := (others => '0');
 end package risc63_pkg;
