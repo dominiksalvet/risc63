a95cefc6472810049c9637e9cec2d603dd62bc1f
Dominik Salvet <dominik.salvet@gmail.com>
2020-07-04 00:59:09 +0200

    add ID multiplexers

--- rtl/id/decoder.vhdl
+++ rtl/id/decoder.vhdl
@@ -13,7 +13,9 @@ entity decoder is
     port (
         i_inst: in std_ulogic_vector(15 downto 0);
 
-        o_iext_opcode: out t_iext_opcode
+        o_iext_opcode: out t_iext_opcode;
+        o_amux_alu: out t_amux_alu;
+        o_bmux_alu: out t_bmux_alu
     );
 end entity decoder;
 
@@ -23,8 +25,8 @@ architecture rtl of decoder is
     signal s_addi_group: std_ulogic; -- addi, addui, auipc, li
     signal s_jz_group: std_ulogic; -- jz, jnz, aipc, jr
     signal s_slti_group: std_ulogic; -- slti, sltui, sgti, sgtui, srli, slli, srai, rsbi
-    signal s_slt_group: std_ulogic; -- slt, sltu, sgt, sgtu, srl, sll, sra, rsb,
-                                    -- sub, and, or, xor, add
+    signal s_add_group: std_ulogic; -- add, sub, and, or, xor,
+                                    -- slt, sltu, sgt, sgtu, srl, sll, sra, rsb,
     signal s_jmp_inst: std_ulogic;
     signal s_extb_group: std_ulogic; -- extb, extw, extd, extbu, extwu, extdu,
                                      -- insb, insw, insd, mskb, mskw, mskd
@@ -39,7 +41,7 @@ begin
     s_addi_group <= '1' when i_inst(15 downto 14) = "10" else '0';
     s_jz_group <= '1' when i_inst(15 downto 13) = "011" else '0';
     s_slti_group <= '1' when i_inst(15 downto 13) = "010" else '0';
-    s_slt_group <= '1' when i_inst(15 downto 12) = "0011" else '0';
+    s_add_group <= '1' when i_inst(15 downto 12) = "0011" else '0';
     s_jmp_inst <= '1' when i_inst(15 downto 12) = "0010" else '0';
     s_extb_group <= '1' when i_inst(15 downto 12) = "0001" else '0';
     s_crr_group <= '1' when i_inst(15 downto 11) = "00001" else '0';
@@ -52,5 +54,17 @@ begin
                      IEXT_JZ when s_jz_group = '1' else
                      IEXT_SLTI when s_slti_group = '1' else
                      IEXT_JMP;
+
+    -- ALU operand A multiplexer
+    o_amux_alu <= AMUX_IMM when s_addi_group = '1' or s_jz_group = '1' or
+                                s_slti_group = '1' or s_jmp_inst = '1' else
+                  AMUX_AREG;
+
+    -- ALU operand B multiplexer
+    o_bmux_alu <= BMUX_IMM when s_ld_group = '1' else
+                  BMUX_PC when (s_addi_group = '1' and i_inst(13 downto 12) = "10") or
+                               s_jz_group = '1' and i_inst(12 downto 11) /= "11" or
+                               s_jmp_inst = '1' else
+                  BMUX_BREG;
 
 end architecture rtl;
--- rtl/risc63_pkg.vhdl
+++ rtl/risc63_pkg.vhdl
@@ -7,11 +7,33 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 package risc63_pkg is
+
+--------------------------------------------------------------------------------
+-- IF STAGE
+--------------------------------------------------------------------------------
+
     -- value of PC after reset
     constant c_PC_RST: std_ulogic_vector(62 downto 0) := (others => '0');
 
+--------------------------------------------------------------------------------
+-- ID STAGE
+--------------------------------------------------------------------------------
+
     -- no operation instruction binary
     constant c_NOP_INST: std_ulogic_vector(15 downto 0) := (others => '0');
+
+    -- immediate extractor
+    type t_iext_opcode is (IEXT_LD, IEXT_ADDI, IEXT_JZ, IEXT_SLTI, IEXT_JMP);
+
+    -- ALU operand A multiplexer
+    type t_amux_alu is (AMUX_IMM, AMUX_AREG);
+
+    -- ALU operand B multiplexer
+    type t_bmux_alu is (BMUX_IMM, BMUX_PC, BMUX_BREG);
+
+--------------------------------------------------------------------------------
+-- EX STAGE
+--------------------------------------------------------------------------------
 
     -- ALU opcodes
     constant c_ALU_ADD: std_ulogic_vector(4 downto 0) := "00000";
@@ -45,8 +67,5 @@ package risc63_pkg is
     constant c_ALU_MSKB: std_ulogic_vector(4 downto 0) := "11100";
     constant c_ALU_MSKW: std_ulogic_vector(4 downto 0) := "11101";
     constant c_ALU_MSKD: std_ulogic_vector(4 downto 0) := "11110";
-
-    -- immediate extractor
-    type t_iext_opcode is (IEXT_LD, IEXT_ADDI, IEXT_JZ, IEXT_SLTI, IEXT_JMP);
 
 end package risc63_pkg;
